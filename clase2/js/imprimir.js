977af2bce5104868c64aabd5e73250837c73d992
Marcos <exegeses@gmail.com>
2021-07-10 12:24:00 -0300

    innerText vs innerHTML

--- clase2/js/imprimir.js
+++ clase2/js/imprimir.js
@@ -1,6 +1,11 @@
 //Ubicamos elemento dentro del DOM
 var txt = document.getElementById('txt');
+var logo = document.getElementById('logo');
 
 //escribimos dentor del elemento span que se llama txt
 txt.innerText = 'texto generado con javascript';
 //document.write('texto primero desde js')
+
+
+//mostramos imagen dentro del figure
+logo.innerHTML = '<img src="https://pluspng.com/img-png/logo-javascript-png-alternate-image-for-javascript-480.png">'
