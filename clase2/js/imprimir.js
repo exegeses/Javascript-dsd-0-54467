8082ce6b86252cfed4b6cb762f69863b7cfb59e3
Marcos <exegeses@gmail.com>
2021-07-10 11:45:53 -0300

    Escribimos usando innerText

---
+++ clase2/js/imprimir.js
@@ -0,0 +1,6 @@
+//Ubicamos elemento dentro del DOM
+var txt = document.getElementById('txt');
+
+//escribimos dentor del elemento span que se llama txt
+txt.innerText = 'texto generado con javascript';
+//document.write('texto primero desde js')
