1d134d80210ed5d7b0aa52bf33d3c13d6ad42433
Marcos <exegeses@gmail.com>
2021-07-17 10:45:04 -0300

    funciones en javascript - función cambiarCSS()

---
+++ clase3/js/funciones.js
@@ -0,0 +1,18 @@
+//ubicamos elemento dentro del DOM
+var caja = document.getElementById('caja');
+
+//código no interactivo
+/*
+caja.style.backgroundColor = '#d00';
+caja.style.width = '300px';
+caja.style.height = '150px';
+*/
+
+//declaramos una función
+function cambiarCSS()
+{
+    caja.style.backgroundColor = '#d00';
+    caja.style.width = '300px';
+    caja.style.height = '150px';
+}
+
